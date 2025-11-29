f0cdcd7b1cbcf26385518e3ec0ba60ca73380b08
Miguel Duarte <miguelmddesign@gmail.com>
2025-11-28 21:49:45 -0300

    novo codigo, quebrado na bomba

--- plantagochi_nov18a.ino
+++ plantagochi_nov18a.ino
@@ -4,7 +4,7 @@
 // Alem disso, tambem dispensaremos a conexao com o celular.
 
 // PINS
-int PINO_BOMBA = 4;
+int PINO_BOMBA = 9;
 int PINO_SENSOR = A1;
 int PINO_BOTAO_REGA = 13;
 
@@ -20,10 +20,11 @@ int estado_atual = 2; //
 
 // variaveis da funcao de regar
 unsigned long ultima_rega = 0;
-unsigned long intervalo = 60000UL * 15; // 15 minutos
+unsigned long intervalo = 60000 * 15; // 15 minutos
 int tempo_rega = 5000; // 5 segundos
 
 void setup() {
+  Serial.begin(9600);
   // SETUP DOS PINS
   pinMode(PINO_SENSOR, INPUT);
   pinMode(PINO_BOMBA, OUTPUT);
@@ -38,16 +39,24 @@ void setup() {
 
 void loop() {
   umidade_atual = lerUmidade();
-  estado_atual = atualizarEstado(umidade_atual);
-  enviarEstadoParaApp(estado_atual);
+  estado_atual = atualizarEstado();
+  enviarEstadoParaApp();
   verificarComandos(); // checa se o user apertou o BOTAO_REGA e liga a bomba dagua
+
+  // Serial.print("Umidade Atual: ");
+  // Serial.println(umidade_atual);
+
+  // Serial.print("Estado atual: ");
+  // Serial.println(estado_atual);
+
+  delay(500);
 }
 
-void atualizarEstado(umidade) {
+int atualizarEstado() {
   int estado;
-  if (umidade < 20) {
+  if (umidade_atual < 20) {
     estado = 1;          // SECO
-  } else if (umidade < 60) {
+  } else if (umidade_atual < 60) {
     estado = 2;          // SAUDAVEL
   } else {
     estado = 3;          // AFOGANDO
@@ -55,9 +64,9 @@ void atualizarEstado(umidade) {
   return estado;
 }
 
-void enviarEstadoParaApp(estado) {
+void enviarEstadoParaApp() {
   // Essa funcao indica o estado da planta com leds, em vez de indicar via notficacao no celular.
-  switch (estado) {
+  switch (estado_atual) {
     case 1: // SECO
       digitalWrite(PINO_LED_1, HIGH);
       digitalWrite(PINO_LED_2, LOW);
@@ -81,33 +90,47 @@ void enviarEstadoParaApp(estado) {
 int lerUmidade() {
   // ler o sensor e mapear a umidade para um valor utilizavel
   int umidade_cru = analogRead(PINO_SENSOR);
-  int umidade = map(umidade_cru, 0, 1023, 0, 100);
+  int umidade = map(umidade_cru, 0, 1023, 255, 0);
   return umidade;
-}
-
-void iniciarRega() {
-  // inicia o ciclo de rega
-  if (umidade_atual < 50 && millis() - ultima_rega > intervalo) { // checa se foi regado a pouco tempo ou se a planta esta seca, para nao enxarcar
-    ligarBomba();
-    delay(tempo_rega);
-    desligarBomba();
-    ultima_rega = millis();
-  }
-}
-
-void ligarBomba() {
-  digitalWrite(PINO_BOMBA, HIGH);
-}
-
-void desligarBomba() {
-  digitalWrite(PINO_BOMBA, LOW);
 }
 
 void verificarComandos() {
   // aqui a gente le o estado do BOTAO_REGA e inicia a rega se tiver apertado
   int botao_rega_estado = digitalRead(PINO_BOTAO_REGA);
   
-  if (botao_rega_estado == HIGH) {
+  if (botao_rega_estado == LOW) {
+    Serial.println("Apertou o botao");
     iniciarRega();
   }
 }
+
+void iniciarRega() {
+  // inicia o ciclo de rega
+  // if (umidade_atual < 50 && millis() - ultima_rega > intervalo) { // checa se foi regado a pouco tempo ou se a planta esta seca, para nao enxarcar
+  //   ligarBomba();
+  //   delay(tempo_rega);
+  //   desligarBomba();
+  //   ultima_rega = millis();
+  // }
+  ligarBomba();
+  delay(5000);
+  desligarBomba();
+  ultima_rega = millis();
+  // if (estado_atual == 1) { // checa se foi regado a pouco tempo ou se a planta esta seca, para nao enxarcar
+  //   ligarBomba();
+  //   delay(10000);
+  //   desligarBomba();
+  //   ultima_rega = millis();
+  // }
+}
+
+void ligarBomba() {
+  Serial.println(" deviria ligar a bomba ");
+  digitalWrite(PINO_BOMBA, HIGH);
+}
+
+void desligarBomba() {
+  Serial.println(" desligar a bomba ");
+  digitalWrite(PINO_BOMBA, LOW);
+}
+
